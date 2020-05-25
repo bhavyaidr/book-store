9d81046e5626fec238a77b13f28a47d5a822bd4d
bhavya121999 <bhavyagupta1203@gmail.com>
2020-05-25 13:07:28 +0530

    1. Completed ProductList component 2.Changed the images in the public/img folder 3. Modified the content of data.js 4. Added context api

--- src/data.js
+++ src/data.js
@@ -1,111 +1,123 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
-    img: "img/product-1.png",
-    price: 10,
-    company: "GOOGLE",
+    title: "The Power of Positive Thinking",
+    img: "img/book1.jpeg",
+    price: 668,
+    author: "Norman Vincent Peale",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "When it was first published, both psychologists and theologians attacked this book as heretical, and accused author Norman Vincent Peale of being a crank. Today, science has verified the book's basic concept--that being optimistic makes you healthier and happier and thus more likely to succeed.  Best quote: Action is a great restorer and builder of confidence. Inaction is not only the result, but the cause, of fear. Perhaps the action you take will be successful; perhaps different action or adjustments will have to follow. But any action is better than no action at all.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 2,
-    title: "Samsung S7",
-    img: "img/product-2.png",
-    price: 16,
-    company: "SAMSUNG",
+    title: "Drive",
+    img: "img/book2.jpeg",
+    price: 339,
+    author: "Daniel H. Pink",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Motivation comes from the judicious use of carrots and sticks, right? Not so, says author Daniel Pink. His book illustrates the fact that motivation comes from many sources and that, at the highest level of performance, motivation come from your deepest and most profound sense of who you are and want to be.Best quote: For artists, scientists, inventors, schoolchildren, and the rest of us, intrinsic motivation--the drive to do something because it is interesting, challenging, and absorbing--is essential for high levels of creativity.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 3,
-    title: "HTC 10 - Black",
-    img: "img/product-3.png",
-    price: 8,
-    company: "htc",
+    title: "Don't Sweat the Small Stuff",
+    img: "img/book3.jpeg",
+    price: 449,
+    author: "Richard Carlson",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "A big part of motivation is clearing your mind of the clutter that weighs it down. Richard Carlson helps you differentiate between what's really important--and deserving of your attention--and what's just noise in the system.Best quote: Stress is nothing more than a socially acceptable form of mental illness.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 4,
-    title: "HTC 10 - White",
-    img: "img/product-4.png",
-    price: 18,
-    company: "htc",
+    title: "The Greatest Salesman in the World",
+    img: "img/book4.jpeg",
+    price: 238,
+    author: "Og Mandino",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Far more than just a sales book, Og Mandino's faux-biblical parable encourages you to live your life to the fullest by believing in yourself and your ability to help others. Required reading for anybody who sells. Which is just about everybody. Best quote: I will live this day as if it is my last. This day is all I have and these hours are now my eternity. I greet this sunrise with cries of joy as a prisoner who is reprieved from death. I lift mine arms with thanks for this priceless gift of a new day.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
-    img: "img/product-5.png",
-    price: 24,
-    company: "htc",
+    title: "As a Man Thinketh",
+    img: "img/book5.jpeg",
+    price: 245,
+    author: "James Allen",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Motivational books are usually about taking immediate action. By contrast, this 1902 classic by James Allen is a bit more, well, thoughtful. It explains how your thoughts mold your personality, and how that personality drives you to take action and determines the type of action you'll take.Best quote: The dreamers are the saviors of the world. As the visible world is sustained by the invisible, so men, through all their trials and sins and sordid vocations, are nourished by the beautiful visions of their solitary dreamers.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 6,
-    title: "Vintage Iphone",
-    img: "img/product-6.png",
-    price: 17,
-    company: "apple",
+    title: "Awaken the Giant Within",
+    img: "img/book6.jpeg",
+    price: 175,
+    author: "Anthony Robbins",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Anthony Robbins is unlike most motivational writers in two ways. First, he treats training the mind and body as a technological challenge rather than moral imperative. Second, he somehow manages to be both an inspirational role model and the most annoying guy on the planet.Best quote: If you can't, you must. If you must, you can.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 7,
-    title: "Iphone 7",
-    img: "img/product-7.png",
-    price: 30,
-    company: "apple",
+    title: "Think and Grow Rich",
+    img: "img/book7.jpeg",
+    price: 300,
+    author: "Napoleon Hill",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Napoleon Hill interviewed 40 millionaires to discover the common thought processes and behaviors that had led to their success. His study of these best practices was revolutionary in a world where it was largely assumed that great wealth resulted from a combination of greed and luck.Best quote: All the breaks you need in life wait within your imagination. Imagination is the workshop of your mind, capable of turning mind energy into accomplishment and wealth.",
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 8,
-    title: "Smashed Iphone",
-    img: "img/product-8.png",
-    price: 2,
-    company: "apple",
+    title: "How to Win Friends & Influence People",
+    img: "img/book8.jpeg",
+    price: 250,
+    author: "Dale Carnegie",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Prior to Dale Carnegie's classic, mixing business relationships with personal relationships was considered a bit underhanded, like nepotism. After Carnegie, business relationships and personal relationships were seen not just as compatible, but as absolutely inseparable.Best quote: The unvarnished truth is that almost all the people you meet feel themselves superior to you in some way, and a sure way to their hearts is to let them realize in some subtle way that you recognize their importance, and recognize it sincerely.",
     inCart: false,
     count: 0,
-    total: 0
-  }
+    total: 0,
+  },
+  {
+    id: 9,
+    title: "The 7 Habits of Highly Effective People",
+    img: "img/book9.jpeg",
+    price: 350,
+    author: "Stephen Covey",
+    info:
+      "While he gets a bit preachy sometimes, Stephen Covey's road map for living doesn't just help you develop better habits. It also convinces you that acquiring them will make you a better person.Best quote: Sow a thought, reap an action; sow an action, reap a habit; sow a habit, reap a character; sow a character, reap a destiny.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
 ];
 
 export const detailProduct = {
   id: 1,
-  title: "Google Pixel - Black",
-  img: "img/product-1.png",
-  price: 10,
-  company: "google",
+  title: "The Power of Positive Thinking",
+  img: "img/book1.jpeg",
+  price: 668,
+  author: "Norman Vincent Peale",
   info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    "When it was first published, both psychologists and theologians attacked this book as heretical, and accused author Norman Vincent Peale of being a crank. Today, science has verified the book's basic concept--that being optimistic makes you healthier and happier and thus more likely to succeed.  Best quote: Action is a great restorer and builder of confidence. Inaction is not only the result, but the cause, of fear. Perhaps the action you take will be successful; perhaps different action or adjustments will have to follow. But any action is better than no action at all.",
   inCart: false,
   count: 0,
-  total: 0
+  total: 0,
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ProductProvider } from "./context";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById("root")
 );
 
