1c5cc168852804d1f000a9e13e411f14438872da
bhavya121999 <bhavyagupta1203@gmail.com>
2020-05-31 16:47:18 +0530

    Changed the format of heading

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -10,7 +10,7 @@ export default class ProductList extends Component {
       <React.Fragment>
         <div className="py-5">
           <div className="container">
-            <Title name="our" title="products" />
+            <Title name="OUR" title=" PRODUCTS " />
             <div className="row">
               <ProductConsumer>
                 {(value) => {
